427c3c3efcc3c6d0c9fd26e424e937eb58774e45
David Feeney <dfeeney@gmail.com>
2011-08-31 18:16:11 -0400

    fixing legacy freemixprofile migrations to reflect changes in the datasource and dataset models that might get run before this migration

--- freemix/legacy/freemixprofile/migrations/0016_require_dataset.py
+++ freemix/legacy/freemixprofile/migrations/0016_require_dataset.py
@@ -87,16 +87,16 @@ class Migration(SchemaMigration):
             'profile': ('django.db.models.fields.TextField', [], {'default': '\'{"properties": []}\''}),
             'published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'db_index': 'True', 'max_length': '50', 'blank': 'True'}),
-            'source': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'datasets'", 'null': 'True', 'to': "orm['dataset.DataSource']"}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
         'dataset.datasource': {
             'Meta': {'object_name': 'DataSource'},
             'classname': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True'}),
             'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'dataset': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['dataset.Dataset']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'data_sources'", 'to': "orm['auth.User']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'data_sources'", 'null': 'True', 'to': "orm['auth.User']"}),
             'uuid': ('django.db.models.fields.CharField', [], {'max_length': '36', 'blank': 'True'})
         },
         'freemixprofile.exhibittheme': {
