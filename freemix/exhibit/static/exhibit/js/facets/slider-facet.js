14c6ae3c6505be88dbd4117d168960f013f187e8
eric miller <ericmiller@cherry.(none)>
2011-11-22 17:21:57 -0500

    adding icons and modifying exhibit editing code for supporting new widget and views (slider, numeric and thumbnail)

---
+++ freemix/exhibit/static/exhibit/js/facets/slider-facet.js
@@ -0,0 +1,82 @@
+/*global jQuery */
+ (function($, Freemix) {
+
+    function isFacetCandidate(prop) {
+        return (prop.values > 1 && prop.values + prop.missing != Freemix.exhibit.database.getAllItemsCount());
+    }
+
+    function simpleSort(a, b) {
+        if (a.missing == b.missing) {
+            return a.values - b.values;
+        } else {
+            return a.missing - b.missing;
+        }
+    }
+
+    function sorter(a, b) {
+        var aIsCandidate = isFacetCandidate(a);
+        var bIsCandidate = isFacetCandidate(b);
+
+        if ((aIsCandidate && bIsCandidate) || (!aIsCandidate && !bIsCandidate)) {
+            return simpleSort(a, b);
+        }
+        return bIsCandidate ? 1: -1;
+    }
+
+
+    function generatePropertyList() {
+        var properties = [];
+        $.each(Freemix.property.enabledProperties(),
+        function(name, property) {
+            properties.push(Freemix.exhibit.getExpressionCount(property.expression(), property.label()));
+        });
+        properties.sort(sorter);
+        return properties;
+    }
+
+    function createPropertyRow(property) {
+       return  $("<tr id='" + property.label + "' class='ui-state-default'>" +
+            "<td>" + property.label + "</td>" +
+            "<td class='right'>" + property.values + "</td>" +
+            "<td class='right'>" + property.missing + "</td></tr>");
+    }
+
+    Freemix.facet.addFacetType({
+        thumbnail: "/static/exhibit/img/slider-facet.png",
+        label: "Slider",
+        config: {
+            type: "Slider",
+	    expression: "",	    
+	    height: "50px"
+        },
+        generateExhibitHTML: function() {
+            return "<div ex:role='facet' ex:facetClass='Slider' ex:histogram='true' ex:horizontal='true' ex:height='50px' ex:expression='" + this.config.expression +
+                "' ex:facetLabel='" + this.config.name + "'></div>";
+        },
+        showEditor: function(facetContainer) {
+            var view = this;
+            var properties = generatePropertyList();
+            var editor = Freemix.getTemplate("list-facet-editor");
+            var table = $("#add-facet-table tbody", editor).empty();
+            $.each(properties,
+            function(i, property) {
+                createPropertyRow(property).click(function() {
+                    view.config.name = property.label;
+                    view.config.expression = property.expression;
+                    facetContainer.hidePopup();
+                    facetContainer.addFacet(view);
+                }).appendTo(table);
+            });
+
+            facetContainer.setPopupContent(editor);
+
+        },
+        generateContent: function() {
+            var count = Freemix.exhibit.getExpressionCount(this.config.expression);
+            return $("<div>Type: <em>" + this.config.type + "</em></div>" +
+                "<div>Expression: <em>" + this.config.expression + "</em></div>" +
+                "<div>Values: <em>" + count.values + "</em></div>" +
+                "<div>Missing: <em>" + count.missing + "</em></div>");
+        }
+    });
+})(window.Freemix.jQuery, window.Freemix);
